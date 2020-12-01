72a62852ab017ce5280fbfec7c3eaf5df19c76d9
Kessler DuPont <43118393+dupontinquiries@users.noreply.github.com>
2020-12-01 12:47:25 -0500

    rolling out new plan

--- thekitchen/js/index.js
+++ thekitchen/js/index.js
@@ -39,42 +39,45 @@ var links = {"data": [
    }
 ]}
 
+/*
+{
+  "name": "Spotify",
+  "description": "Listen to songs from The Kitchen and featured artists.",
+  "image": "images/spotify-header.png",
+  "link": "https://open.spotify.com/artist/3UZDsnAmnpGlFGlFyikvIt?si=UuOryZueRL2hSHIA4IRopg",
+  "songDemo" : false
+},
+{
+  "name": "Apple Music",
+  "description": "Listen to songs from The Kitchen and featured artists.",
+  "image": "images/applemusic.png",
+  "link": "https://music.apple.com/us/artist/the-kitchen/1480129249",
+  "songDemo" : false
+},
+{
+  "name": "YouTube",
+  "description": "Music videos & more await.",
+  "image": "images/youtube.png",
+  "link": "https://www.youtube.com/channel/UCw7YTR6oSIan42thJCtWC9g",
+  "songDemo" : false
+},
+{
+  "name": "BeatStars",
+  "description": "Pick up some fire beats.",
+  "image": "images/beatstars.jpg",
+  "link": "https://www.beatstars.com/yokthatscrazy",
+  "songDemo" : false
+},
+{
+  "name": "COVID-19 Fundraiser",
+  "description": "Support The Rocket Initiative, our fight against COVID-19.",
+  "image": "images/avengers.jpg",
+  "link": "https://www.beatstars.com/yokthatscrazy",
+  "songDemo" : false
+},
+*/
+
 var items = {"data": [
-  {
-    "name": "Spotify",
-    "description": "Listen to songs from The Kitchen and featured artists.",
-    "image": "images/spotify.jpg",
-    "link": "https://open.spotify.com/artist/3UZDsnAmnpGlFGlFyikvIt?si=UuOryZueRL2hSHIA4IRopg",
-    "songDemo" : false
-  },
-  {
-    "name": "Apple Music",
-    "description": "Listen to songs from The Kitchen and featured artists.",
-    "image": "images/applemusic.png",
-    "link": "https://music.apple.com/us/artist/the-kitchen/1480129249",
-    "songDemo" : false
-  },
-  {
-    "name": "YouTube",
-    "description": "Music videos & more await.",
-    "image": "images/youtube.png",
-    "link": "https://www.youtube.com/channel/UCw7YTR6oSIan42thJCtWC9g",
-    "songDemo" : false
-  },
-  {
-    "name": "BeatStars",
-    "description": "Pick up some fire beats.",
-    "image": "images/beatstars.jpg",
-    "link": "https://www.beatstars.com/yokthatscrazy",
-    "songDemo" : false
-  },
-  {
-    "name": "COVID-19 Fundraiser",
-    "description": "Support The Rocket Initiative, our fight against COVID-19.",
-    "image": "images/avengers.jpg",
-    "link": "https://www.beatstars.com/yokthatscrazy",
-    "songDemo" : false
-  },
   {
     "name": "The Kitchen",
     "description": "Coming soon!",
@@ -83,21 +86,37 @@ var items = {"data": [
     "songDemo" : false
   },
   {
-    "name": "TripLit",
+    "name": "TripLit - Featured Artist",
     "description": "Coming soon!",
     "image": "images/people/triplit.jpg",
     "link": "https://dupontinquiries.github.io/triplit",
     "songDemo" : false
   },
   {
-    "name": "SJ",
+    "name": "SJ - Featured Artist",
     "description": "Coming soon!",
     "image": "images/people/sj.jpg",
     "link": "https://www.instagram.com/sj.menon",
     "songDemo" : false
   },
   {
-    "name": "Run Up",
+    "name": "Fire Keep - Instrumental",
+    "description": "The Kitchen - Fire Keep is available on all platforms.",
+    "image": "images/songs/firekeep.jpg",
+    "link": "https://open.spotify.com/track/69247gEWDwt7HVWxRCUdja?si=q3JVZs6rS4ulzFlViGlTvQ",
+    "embed" : true,
+    "embedCode" : "<iframe src=\"https://open.spotify.com/embed/track/69247gEWDwt7HVWxRCUdja\" width=\"300\" height=\"80\" frameborder=\"0\" allowtransparency=\"true\" allow=\"encrypted-media\"></iframe>"
+  },
+  {
+    "name": "Angel - Instrumental",
+    "description": "The Kitchen - Angel is available on all platforms.",
+    "image": "images/songs/angel.jpg",
+    "link": "https://open.spotify.com/track/0wX3BuEBY2ahao3bSHslD4?si=r5hXSq7vSO2r8tcrrnoMcw",
+    "embed" : true,
+    "embedCode" : "<iframe src=\"https://open.spotify.com/embed/track/0wX3BuEBY2ahao3bSHslD4\" width=\"300\" height=\"80\" frameborder=\"0\" allowtransparency=\"true\" allow=\"encrypted-media\"></iframe>"
+  },
+  {
+    "name": "Run Up (feat. SJ)",
     "description": "The Kitchen - Run Up (feat. SJ) is available on all platforms.",
     "image": "images/songs/runup.jpg",
     "link": "https://open.spotify.com/track/3rwRLRQdAqNFmc5mG4wsa2?si=y-2z5-zOR4SZ2U2WjgTRww",
@@ -105,7 +124,7 @@ var items = {"data": [
     "audioFile" : "audio/runup.mp3"
   },
   {
-    "name": "Rho",
+    "name": "Rho (feat. SJ)",
     "description": "The Kitchen - Rho (feat. SJ) is available on all platforms.",
     "image": "images/songs/rho.jpg",
     "link": "https://open.spotify.com/track/12VX4vJ4T1JhFMN3BirYzu?si=d0QBFUS3Tv29ye6YkE8kvw",
@@ -113,7 +132,7 @@ var items = {"data": [
     "audioFile" : "audio/rho.mp3"
   },
   {
-    "name": "All My Life",
+    "name": "All My Life (feat. TripLit)",
     "description": "The Kitchen - All My Life (feat. TripLit) is available on all platforms.",
     "image": "images/songs/allmylife.jpg",
     "link": "https://open.spotify.com/track/2XfOcve72EWAVC4PBVQs1D?si=h0csgpFOR5eQtV_mQDgL-g",
@@ -121,7 +140,7 @@ var items = {"data": [
     "audioFile" : "audio/aml.mp3"
   },
   {
-    "name": "Bars",
+    "name": "Bars (feat. SJ)",
     "description": "The Kitchen - Bars (feat. SJ) is available on all platforms.",
     "image": "images/songs/bars.jpg",
     "link": "https://open.spotify.com/track/5iMD9mnkWyIcJDoACq3B8W?si=SmAfOUCMQCeWBWgHrjwt8g",
@@ -129,7 +148,7 @@ var items = {"data": [
     "audioFile" : "audio/bars.mp3"
   },
   {
-    "name": "Known",
+    "name": "Known (feat. SJ)",
     "description": "The Kitchen - Known (feat. SJ) is available on all platforms.",
     "image": "images/songs/known.jpg",
     "link": "https://open.spotify.com/track/0us3QPqtjr5N0vnMFxvI3y?si=nSSM8oLQQM2PeSxNUsZxhw",
@@ -137,12 +156,20 @@ var items = {"data": [
     "audioFile" : "audio/known.mp3"
   },
   {
-    "name": "I Don't Know You Tell Me",
+    "name": "I Don't Know You Tell Me (feat. Eric Weir)",
     "description": "Eric Weir's debut album, I Don't Know You Tell Me, is available on all platforms.",
     "image": "images/songs/idkytm.jpg",
     "link": "https://open.spotify.com/album/2d4iFet1Qw8vCQmcNm75iT?si=eZUu2PrbTLuT6cw_YqPClw",
     "songDemo" : true,
     "audioFile" : "audio/idkytm.mp3"
+  },
+  {
+    "name": "Merchandise",
+    "description": "Coming soon.",
+    "image": "images/tshirt.jpg",
+    "link": "",
+    "songDemo" : false,
+    "embed": false
   }
 ]
 }
@@ -167,7 +194,10 @@ for ( var i = 0; i < items.data.length; ++i ) {
   // // // //
 
   //console.log(it.name);
-  if ( it.songDemo ) {
+  if ( it.embed ) {
+    html += "<div class=\"col s12 m6 l6\"><div class=\"card hoverable\"><div class=\"card-image waves-effect waves-block waves-light\"><img class=\"activator\" src=" + it.image + "></div><div class=\"card-content\"><span class=\"card-title activator grey-text text-darken-4\">" + it.name + "<i class=\"material-icons right\">keyboard_arrow_up</i></span><p><a href=" + it.link + " target=\"_blank\">Listen</a></p></div><div class=\"card-reveal\"><span class=\"card-title grey-text text-darken-4\">" + it.name + "<i class=\"material-icons right\">close</i></span><p>" + it.description;
+    html += "<br><br>" + it.embedCode + "</p></div></div></div>";
+  } else if ( it.songDemo ) {
     html += "<div class=\"col s12 m6 l6\"><div class=\"card hoverable\"><div class=\"card-image waves-effect waves-block waves-light\"><img class=\"activator\" src=" + it.image + "></div><div class=\"card-content\"><span class=\"card-title activator grey-text text-darken-4\">" + it.name + "<i class=\"material-icons right\">keyboard_arrow_up</i></span><p><a href=" + it.link + " target=\"_blank\">Listen</a></p></div><div class=\"card-reveal\"><span class=\"card-title grey-text text-darken-4\">" + it.name + "<i class=\"material-icons right\">close</i></span><p>" + it.description;
     html += "<br>" + "<p><audio controls><source src=\"" + it.audioFile + "\" type=\"audio/mpeg\">Your browser does not support the audio element.</audio></p>" + "</p></div></div></div>";
   } else {
