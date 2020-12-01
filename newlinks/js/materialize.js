49a8873b43539d5ab59ef92ae5a1df915d195fa4
Kessler DuPont <43118393+dupontinquiries@users.noreply.github.com>
2020-11-30 20:36:45 -0500

    Update materialize.js

--- newlinks/js/materialize.js
+++ newlinks/js/materialize.js
@@ -5447,31 +5447,6 @@ if (Vel) {
 
     $(document).on('click.card', '.card', function (e) {
 
-      // close all other tabs
-      ///*
-      $('.card').each(function () {
-        if ($(this) ) {
-          if ( $(this) == e.target ) {
-
-          } else {
-            $(this).find('.card-reveal').velocity({ translateY: 0 }, {
-              duration: 100,
-              queue: false,
-              easing: 'easeInOutQuad',
-              complete: function () {
-                $(this).css({ display: 'none' });
-                $(this).css('overflow', $card.data('initialOverflow'));
-              }
-            });
-            //console.log ( 'toggline (' + 0 + ')' );
-            if ( $(this).find('.activator').hasClass('blurred-image') ) {
-              $(this).find('.activator').toggleClass('blurred-image');
-            }
-          }
-        }
-      });
-      //*/
-
       if ($(this).find('> .card-reveal').length) {
         var $card = $(e.target).closest('.card');
         if ($card.data('initialOverflow') === undefined) {
@@ -5491,6 +5466,30 @@ if (Vel) {
           //console.log ( 'toggline (' + 0 + ')' );
           $(this).find('.activator').toggleClass('blurred-image');
         } else if ($(e.target).is($('.card .activator')) || $(e.target).is($('.card .activator i'))) {
+          // close all other tabs
+          ///*
+          $('.card').each(function () {
+            if ($(this) ) {
+              if ( $(this) == e.target ) {
+
+              } else {
+                $(this).find('.card-reveal').velocity({ translateY: 0 }, {
+                  duration: 100,
+                  queue: false,
+                  easing: 'easeInOutQuad',
+                  complete: function () {
+                    $(this).css({ display: 'none' });
+                    $(this).css('overflow', $card.data('initialOverflow'));
+                  }
+                });
+                //console.log ( 'toggline (' + 0 + ')' );
+                if ( $(this).find('.activator').hasClass('blurred-image') ) {
+                  $(this).find('.activator').toggleClass('blurred-image');
+                }
+              }
+            }
+          });
+          //*/
           $card.css('overflow', 'hidden');
           $(this).find('.card-reveal').css({ display: 'block' }).velocity("stop", false).velocity({ translateY: '-100%' }, { duration: 200, queue: false, easing: 'easeInOutQuad' });
           //console.log ( 'toggline (' + 1 + ')' );
