c05f5316beaaa80ee3759806162ae06c2623d82d
Kessler DuPont <43118393+dupontinquiries@users.noreply.github.com>
2020-12-05 00:09:04 -0500

    cleaned up wksp

--- thekitchen/js/index_.js
+++ thekitchen/js/index_.js
@@ -39,6 +39,43 @@ var links = {"data": [
    }
 ]}
 
+/*
+{
+  "name": "Spotify",
+  "description": "Listen to songs from The Kitchen and featured artists.",
+  "image": "images/spotify-header.png",
+  "link": "https://open.spotify.com/artist/3UZDsnAmnpGlFGlFyikvIt?si=UuOryZueRL2hSHIA4IRopg",
+  "songDemo" : false
+},
+{
+  "name": "Apple Music",
+  "description": "Listen to songs from The Kitchen and featured artists.",
+  "image": "images/applemusic.png",
+  "link": "https://music.apple.com/us/artist/the-kitchen/1480129249",
+  "songDemo" : false
+},
+{
+  "name": "YouTube",
+  "description": "Music videos & more await.",
+  "image": "images/youtube.png",
+  "link": "https://www.youtube.com/channel/UCw7YTR6oSIan42thJCtWC9g",
+  "songDemo" : false
+},
+{
+  "name": "BeatStars",
+  "description": "Pick up some fire beats.",
+  "image": "images/beatstars.jpg",
+  "link": "https://www.beatstars.com/yokthatscrazy",
+  "songDemo" : false
+},
+{
+  "name": "COVID-19 Fundraiser",
+  "description": "Support The Rocket Initiative, our fight against COVID-19.",
+  "image": "images/avengers.jpg",
+  "link": "https://www.beatstars.com/yokthatscrazy",
+  "songDemo" : false
+},
+*/
 
 var items = {"data": [
   {
@@ -63,7 +100,7 @@ var items = {"data": [
     "songDemo" : false
   },
   {
-    "name": "The Kitchen - Fire Keep",
+    "name": "Fire Keep - Instrumental",
     "description": "The Kitchen - Fire Keep is available on all platforms.",
     "image": "images/songs/firekeep.jpg",
     "link": "https://open.spotify.com/track/69247gEWDwt7HVWxRCUdja?si=q3JVZs6rS4ulzFlViGlTvQ",
@@ -71,7 +108,7 @@ var items = {"data": [
     "embedCode" : "<iframe src=\"https://open.spotify.com/embed/track/69247gEWDwt7HVWxRCUdja\" width=\"300\" height=\"80\" frameborder=\"0\" allowtransparency=\"true\" allow=\"encrypted-media\"></iframe>"
   },
   {
-    "name": "The Kitchen - Angel",
+    "name": "Angel - Instrumental",
     "description": "The Kitchen - Angel is available on all platforms.",
     "image": "images/songs/angel.jpg",
     "link": "https://open.spotify.com/track/0wX3BuEBY2ahao3bSHslD4?si=r5hXSq7vSO2r8tcrrnoMcw",
@@ -182,3 +219,47 @@ document.addEventListener('play', function(e){
         }
     }
 }, true);
+
+/*
+
+var work = document.getElementById("portfolioItemContainer");
+var workRequest = new XMLHttpRequest();
+
+var link = document.getElementById("socialLinkContent");
+var linkRequest = new XMLHttpRequest();
+
+linkRequest.open("GET", "json/links.json", true);
+linkRequest.onreadystatechange = function () {
+    if (linkRequest.readyState === 4) {
+        if (linkRequest.status === 200 || linkRequest.status == 0) {
+            var html = "";
+            JSON.parse(linkRequest.responseText).forEach(function (link) {
+                console.log(link.name);
+                html += "<div class=\"links waves-effect\"><a href=" + link.url + " class=\"black-text valign-wrapper\" target=\"_blank\"><i class=\"link_icon mdi mdi-" + link.icon + "\" style=\"color: " + link.color + "\"></i>&nbsp; " + link.name + "</a></div>";
+            });
+            link.innerHTML = html;
+        }
+    }
+};
+linkRequest.send(null);
+
+workRequest.open("GET", "json/items.json", true);
+workRequest.onreadystatechange = function () {
+    if (workRequest.readyState === 4) {
+        if (workRequest.status === 200 || workRequest.status == 0) {
+            var html = "";
+            JSON.parse(workRequest.responseText).forEach(function (work) {
+                console.log(work.name);
+                if ( link.songDemo ) {
+                  html += "<div class=\"col s12 m6 l6\"><div class=\"card hoverable\"><div class=\"card-image waves-effect waves-block waves-light\"><img class=\"activator\" src=" + work.image + "></div><div class=\"card-content\"><span class=\"card-title activator grey-text text-darken-4\">" + work.name + "<i class=\"material-icons right\">keyboard_arrow_up</i></span><p><a href=" + work.link + " target=\"_blank\">Visit</a></p></div><div class=\"card-reveal\"><span class=\"card-title grey-text text-darken-4\">" + work.name + "<i class=\"material-icons right\">close</i></span><p>" + work.description + "<br>" + link.songDemoURL + "</p></div></div></div>";
+                } else {
+                  html += "<div class=\"col s12 m6 l6\"><div class=\"card hoverable\"><div class=\"card-image waves-effect waves-block waves-light\"><img class=\"activator\" src=" + work.image + "></div><div class=\"card-content\"><span class=\"card-title activator grey-text text-darken-4\">" + work.name + "<i class=\"material-icons right\">keyboard_arrow_up</i></span><p><a href=" + work.link + " target=\"_blank\">Visit</a></p></div><div class=\"card-reveal\"><span class=\"card-title grey-text text-darken-4\">" + work.name + "<i class=\"material-icons right\">close</i></span><p>" + work.description + "</p></div></div></div>";
+                }
+            });
+            work.innerHTML = html;
+        }
+    }
+};
+workRequest.send(null);
+
+*/
