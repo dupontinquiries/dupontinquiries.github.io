a76f7d6d34cf31fccb5916287f16173684950823
Kessler DuPont <43118393+dupontinquiries@users.noreply.github.com>
2022-01-21 17:43:59 -0500

    thekitchen

---
+++ thekitchen/js/index.js
@@ -0,0 +1,310 @@
+
+
+var links = {"data": [
+  {
+      "name":"Support (coming soon)",
+      "color": "gray",
+      "icon": "currency-eth",
+      "url":"#"
+  },
+  {
+      "name":"YouTube",
+      "color":"red",
+      "icon":"video",
+      "url":"https://www.youtube.com/channel/UCw7YTR6oSIan42thJCtWC9g"
+  },
+  {
+      "name":"Spotify",
+      "color":"green",
+      "icon":"spotify",
+      "url":"https://open.spotify.com/artist/3UZDsnAmnpGlFGlFyikvIt?si=UuOryZueRL2hSHIA4IRopg"
+  },
+  {
+      "name":"Apple Music",
+      "color":"black",
+      "icon":"apple",
+      "url":"https://music.apple.com/us/artist/the-kitchen/1480129249"
+  }
+]}
+
+
+var items = {"data": [
+  {
+    "name": "Troy, Vol. 2",
+    "description": "The Troy, Vol. 2 beat tape is coming soon to all platforms.",
+    "image": "images/songs/troy2.jpg",
+    "link": "",
+    "useSpotifyLink": true,
+    "spotifyLink": "https://open.spotify.com/album/6cTrs5jiKnseKwTM7xp3i4",
+    "useAppleLink": true,
+    "appleLink": "https://music.apple.com/us/album/troy-vol-2/1602805385",
+    "useYouTubeLink": true,
+    "youtubeLink": "https://www.youtube.com/playlist?list=PL8empeWnnQ6myylsdzjIu4cQubmzVRFuw",
+    "songDemo" : true,
+    // "audioFile" : "audio/takeoff.mp3"
+  },
+  {
+    "name": "Beat Tape: QC3",
+    "description": "The Quarantine Cookups, Vol. 2 beat tape is available all platforms.",
+    "image": "images/songs/qc3.jpg",
+    "link": "",
+    "useSpotifyLink": true,
+    "spotifyLink": "https://open.spotify.com/album/5uJJdRK2amMwWjrTLjyQQW?si=piD8HoJLRlCmI3J8HoS2vA",
+    "useAppleLink": true,
+    "appleLink": "https://music.apple.com/us/album/quarantine-cookups-vol-3/1596108553",
+    "useYouTubeLink": true,
+    "youtubeLink": "https://www.youtube.com/playlist?list=PL8empeWnnQ6nMsiUCk1VHY1_VkH6uYR2i",
+    "songDemo" : true,
+    // "audioFile" : "audio/takeoff.mp3"
+  },
+  {
+    "name": "Beat Tape: QC2",
+    "description": "The Quarantine Cookups, Vol. 2 beat tape is available all platforms.",
+    "image": "images/songs/qc2.jpg",
+    "link": "",
+    "useSpotifyLink": true,
+    "spotifyLink": "https://open.spotify.com/album/1ZFWXE1qsUBW5zR8QYGGhJ?si=2zDvJINZQJ6R-QJ0Uvh9Mg",
+    "useAppleLink": true,
+    "appleLink": "https://music.apple.com/us/album/quarantine-cookups-vol-2/1590524263",
+    "useYouTubeLink": true,
+    "youtubeLink": "https://youtube.com/playlist?list=PL8empeWnnQ6lFCid7kAjQWaFLf4rtStln",
+    "songDemo" : true,
+    // "audioFile" : "audio/takeoff.mp3"
+  },
+  {
+    "name": "Beat Tape: Green Island",
+    "description": "Green Island beat tape coming soon to all platforms.",
+    "image": "images/songs/greenisland.jpg",
+    "link": "",
+    "useSpotifyLink": true,
+    "spotifyLink": "https://open.spotify.com/album/5COC7zoOohB4la9TCn4jui?si=YqeBLTrwSQyahaQfjEhaBQ&dl_branch=1",
+    "useAppleLink": true,
+    "appleLink": "https://music.apple.com/us/album/green-island/1570665645",
+    "useYouTubeLink": true,
+    "youtubeLink": "https://youtube.com/playlist?list=PL8empeWnnQ6m2qu26ychP9PE09o5INT8L",
+    "songDemo" : true,
+    "audioFile" : "audio/takeoff.mp3"
+  },
+  {
+    "name": "Blue Moon (feat. A.M.)",
+    "description": "Blue Moon (feat. A.M.) is available on all platforms.",
+    "image": "images/songs/bluemoon.jpg",
+    "link": "",
+    "useSpotifyLink": true,
+    "spotifyLink": "https://open.spotify.com/album/1WIC4LqDnQS8jI1l7xPMyN?si=nqDJXmppTsa9mzvm0lFuRA",
+    "useAppleLink": true,
+    "appleLink": "https://music.apple.com/us/album/blue-moon-feat-am-single/1565864783",
+    "useYouTubeLink": true,
+    "youtubeLink": "https://youtu.be/9-9fN415e_o",
+    "songDemo" : true,
+    "audioFile" : "audio/bluemoondemo.mp3"
+  },
+  {
+    "name": "Neptune",
+    "description": "Neptune is a sneak peek at the Green Island beat tape, which is available now on all platforms.",
+    "image": "images/songs/neptune.jpg",
+    "link": "",
+    "useSpotifyLink": true,
+    "spotifyLink": "https://open.spotify.com/album/4QnrjKUIronRYliXKUjEfW?si=8_B1QUnFT4WQagjT9eNOQA&dl_branch=1",
+    "useAppleLink": true,
+    "appleLink": "https://music.apple.com/us/album/neptune-single/1569554801",
+    "useYouTubeLink": true,
+    "youtubeLink": "https://www.youtube.com/watch?v=IyKJOHP0gmo&list=PL8empeWnnQ6m2qu26ychP9PE09o5INT8L&index=5",
+    "songDemo" : true,
+    "audioFile" : "audio/neptune.mp3"
+  },
+  {
+    "name": "Beat Tape: Quarantine Cookups",
+    "description": "Quarantine Cookups is available on Spotify & YouTube.",
+    "image": "images/klogo.jpg",
+    "link": "",
+    "useSpotifyLink": true,
+    "spotifyLink": "https://open.spotify.com/album/4ztdzogpONJn0JJ1TJFHrt?si=IpqUm2oKQxytMzzD-Lm9dg&dl_branch=1",
+    "useAppleLink": true,
+    "appleLink": "https://music.apple.com/us/album/quarantine-cookups/1546342063",
+    "useYouTubeLink": true,
+    "youtubeLink": "https://www.youtube.com/watch?v=aHR14w2-Kh0&list=PL8empeWnnQ6nXNoJlwv9Fgcs1RQzmRlS0",
+    "songDemo" : true,
+  },
+  {
+    "name": "The Kitchen - Fire Keep",
+    "description": "The Kitchen - Fire Keep is available on all platforms.",
+    "image": "images/songs/firekeep.jpg",
+    "link": "https://open.spotify.com/track/69247gEWDwt7HVWxRCUdja?si=q3JVZs6rS4ulzFlViGlTvQ",
+    "embed" : true,
+    "embedCode" : "<iframe src=\"https://open.spotify.com/embed/track/69247gEWDwt7HVWxRCUdja\" width=\"300\" height=\"80\" frameborder=\"0\" allowtransparency=\"true\" allow=\"encrypted-media\"></iframe>"
+  },
+  {
+    "name": "The Kitchen - Angel",
+    "description": "The Kitchen - Angel is available on all platforms.",
+    "image": "images/songs/angel.jpg",
+    "link": "https://open.spotify.com/track/0wX3BuEBY2ahao3bSHslD4?si=r5hXSq7vSO2r8tcrrnoMcw",
+    "embed" : true,
+    "embedCode" : "<iframe src=\"https://open.spotify.com/embed/track/0wX3BuEBY2ahao3bSHslD4\" width=\"300\" height=\"80\" frameborder=\"0\" allowtransparency=\"true\" allow=\"encrypted-media\"></iframe>"
+  },
+  {
+    "name": "Run Up (feat. SJ)",
+    "description": "The Kitchen - Run Up (feat. SJ) is available on all platforms.",
+    "image": "images/songs/runup.jpg",
+    "link": "",
+    "useSpotifyLink": true,
+    "spotifyLink": "https://open.spotify.com/track/3rwRLRQdAqNFmc5mG4wsa2?si=y-2z5-zOR4SZ2U2WjgTRww",
+    "useAppleLink": true,
+    "appleLink": "https://music.apple.com/us/album/run-up-feat-sj/1539330796?i=1539330797",
+    "useYouTubeLink": true,
+    "youtubeLink": "https://www.youtube.com/watch?v=LgYwdx6bFJE&list=PL8empeWnnQ6lyQ2dfo0RV7eGg614v0wYu&index=1",
+    "songDemo" : true,
+    "audioFile" : "audio/runup.mp3"
+  },
+  {
+    "name": "Rho (feat. SJ)",
+    "description": "The Kitchen - Rho (feat. SJ) is available on all platforms.",
+    "image": "images/songs/rho.jpg",
+    "link": "",
+    "useSpotifyLink": true,
+    "spotifyLink": "https://open.spotify.com/track/12VX4vJ4T1JhFMN3BirYzu?si=d0QBFUS3Tv29ye6YkE8kvw",
+    "useAppleLink": true,
+    "appleLink": "https://music.apple.com/us/album/rho-feat-sj/1517720924?i=1517720927",
+    "useYouTubeLink": true,
+    "youtubeLink": "https://www.youtube.com/watch?v=LpzGY9OubQ4&list=PL8empeWnnQ6lyQ2dfo0RV7eGg614v0wYu&index=2",
+    "songDemo" : true,
+    "audioFile" : "audio/rho.mp3"
+  },
+  {
+    "name": "All My Life (feat. TripLit)",
+    "description": "The Kitchen - All My Life (feat. TripLit) is available on all platforms.",
+    "image": "images/songs/allmylife.jpg",
+    "link": "https://open.spotify.com/track/2XfOcve72EWAVC4PBVQs1D?si=h0csgpFOR5eQtV_mQDgL-g",
+    "songDemo" : true,
+    "audioFile" : "audio/aml.mp3"
+  },
+  {
+    "name": "Bars (feat. SJ)",
+    "description": "The Kitchen - Bars (feat. SJ) is available on all platforms.",
+    "image": "images/songs/bars.jpg",
+    "link": "",
+    "useSpotifyLink": true,
+    "spotifyLink": "https://open.spotify.com/track/5iMD9mnkWyIcJDoACq3B8W?si=SmAfOUCMQCeWBWgHrjwt8g",
+    "useAppleLink": true,
+    "appleLink": "https://music.apple.com/us/album/bars-feat-sj/1502033165?i=1502033408",
+    "useYouTubeLink": true,
+    "youtubeLink": "https://www.youtube.com/watch?v=h11Q_P26k44&list=PL8empeWnnQ6lyQ2dfo0RV7eGg614v0wYu&index=4",
+    "songDemo" : true,
+    "audioFile" : "audio/bars.mp3"
+  },
+  {
+    "name": "Known (feat. SJ)",
+    "description": "The Kitchen - Known (feat. SJ) is available on all platforms.",
+    "image": "images/songs/known.jpg",
+    "link": "",
+    "useSpotifyLink": true,
+    "spotifyLink": "https://open.spotify.com/track/0us3QPqtjr5N0vnMFxvI3y?si=nSSM8oLQQM2PeSxNUsZxhw",
+    "useAppleLink": true,
+    "appleLink": "https://music.apple.com/us/album/known-feat-sj/1480132960?i=1480132965",
+    "useYouTubeLink": true,
+    "youtubeLink": "https://www.youtube.com/watch?v=fptBgfJElEo&list=PL8empeWnnQ6lyQ2dfo0RV7eGg614v0wYu&index=5",
+    "songDemo" : true,
+    "audioFile" : "audio/known.mp3"
+  },
+  {
+    "name": "I Don't Know You Tell Me (feat. Eric Weir)",
+    "description": "Eric Weir's debut album, I Don't Know You Tell Me, is available on all platforms.",
+    "image": "images/songs/idkytm.jpg",
+    "link": "https://open.spotify.com/album/2d4iFet1Qw8vCQmcNm75iT?si=eZUu2PrbTLuT6cw_YqPClw",
+    "songDemo" : true,
+    "audioFile" : "audio/idkytm.mp3"
+  }
+]
+}
+
+
+
+//links.data = shuffle(links.data);
+items.data = shuffle(items.data);
+
+function shuffle(arr) {
+    for (var i = 1; i < arr.length; i++) {
+      // get random index
+      var j = i + Math.floor(Math.random() * (arr.length - i));
+      // shuffle
+      var temp = arr[j];
+      arr[j] = arr[i];
+      arr[i] = temp;
+      //console.log(i + ", " + j);
+    }
+    return arr;
+}
+
+var html = "";
+var html1 = "";
+var html2 = "";
+for ( var i = 0; i < links.data.length; ++i ) {
+  var it = links.data[i];
+
+  // // // //
+
+  //console.log(it.name);
+  html += "<div class=\"links waves-effect\"><a href=" + it.url + " class=\"black-text valign-wrapper\" target=\"_blank\"><i class=\"link_icon mdi mdi-" + it.icon + "\" style=\"color: " + it.color + "\"></i>&nbsp; " + it.name + "</a></div>";
+
+  // // // //
+}
+document.getElementById("socialLinkContent").innerHTML = html;
+
+htm = "";
+html1 = "";
+html2 = "";
+for ( var i = 0; i < items.data.length; ++i ) {
+  var tmp = "";
+  var it = items.data[i];
+  // // // //
+
+  //console.log(it.name);
+  if ( it.embed ) {
+    tmp += "<div class=\"col s12 m6 l6\"><div class=\"card hoverable\"><div class=\"card-image waves-effect waves-block waves-light\"><img class=\"activator\" src=" + it.image + "></div><div class=\"card-content\"><span class=\"card-title activator grey-text text-darken-4\">" + it.name + "<i class=\"material-icons right\">keyboard_arrow_up</i></span><p><a href=" + it.link + " target=\"_blank\">Listen</a></p></div><div class=\"card-reveal\"><span class=\"card-title grey-text text-darken-4\">" + it.name + "<i class=\"material-icons right\">close</i></span><p>" + it.description;
+    tmp += "<br><br>" + it.embedCode + "</p></div></div></div>";
+  } else if ( it.songDemo ) {
+    tmp += "<div class=\"col s12 m6 l6\"><div class=\"card hoverable\"><div class=\"card-image waves-effect waves-block waves-light\"><img class=\"activator\" src=" + it.image + "></div><div class=\"card-content\"><span class=\"card-title activator grey-text text-darken-4\">" + it.name + "<i class=\"material-icons right\">keyboard_arrow_up</i></span><p>";
+    if ( it.useSpotifyLink ) {
+      tmp += "<a href=" + it.spotifyLink + " target=\"_blank\"><i class=\"link_icon mdi mdi-" + "spotify" + "\" style=\"color: " + "black" + "\"></i></a>"
+    }
+    if ( it.useAppleLink ) {
+      tmp += "<a href=" + it.appleLink + " target=\"_blank\"><i class=\"link_icon mdi mdi-" + "apple" + "\" style=\"color: " + "black" + "\"></i></a>"
+    }
+    if ( it.useYouTubeLink ) {
+      tmp += "<a href=" + it.youtubeLink + " target=\"_blank\"><i class=\"link_icon mdi mdi-" + "video" + "\" style=\"color: " + "black" + "\"></i></a>"
+    }
+    if ( it.useSpotifyLink || it.useAppleLink || it.useYouTubeLink ) {
+    }
+    else {
+      + "<a href=" + it.link + " target=\"_blank\">Listen</a>";
+    }
+    tmp += "</p></div><div class=\"card-reveal\"><span class=\"card-title grey-text text-darken-4\">" + it.name + "<i class=\"material-icons right\">close</i></span><p>" + it.description;
+    tmp += "<br>" + "<p><audio controls><source src=\"" + it.audioFile + "\" type=\"audio/mpeg\">Your browser does not support the audio element.</audio></p>" + "</p></div></div></div>";
+  } else {
+    //var extravisit = "<p><a href=" + it.link + " target=\"_blank\">Visit</a></p>";
+    var extravisit = "";
+    tmp += "<div class=\"col s12 m6 l6\"><div class=\"card hoverable\"><div class=\"card-image waves-effect waves-block waves-light\"><img class=\"activator\" src=" + it.image + "></div><div class=\"card-content\"><span class=\"card-title activator grey-text text-darken-4\">" + it.name + "<i class=\"material-icons right\">keyboard_arrow_up</i></span><p><a href=" + it.link + " target=\"_blank\">Visit</a></p></div><div class=\"card-reveal\"><span class=\"card-title grey-text text-darken-4\">" + it.name + "<i class=\"material-icons right\">close</i></span><p>" + it.description + "</p>" + extravisit + "</div></div></div>";
+  }
+
+  if ( i < items.data.length / 2 ) {
+    html1 += tmp;
+  } else {
+    html1 += tmp;
+  }
+  // // // //
+
+
+}
+// html += html1 + html2;
+//document.getElementById("portfolioItemContainer").innerHTML = html;
+document.getElementById("pic1").innerHTML = html1;
+// document.getElementById("pic2").innerHTML = html2;
+
+document.addEventListener('play', function(e){
+    var audios = document.getElementsByTagName('audio');
+    for(var i = 0, len = audios.length; i < len;i++){
+        if(audios[i] != e.target){
+            audios[i].pause();
+        }
+    }
+}, true);
